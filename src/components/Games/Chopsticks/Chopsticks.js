41680c05f80d9c8d06d3a82f9f422ab42d482b98
Sidharth Giri <sid.iiith@outlook.com>
2020-11-19 22:20:47 +0530

    Added Content to Chopsticks

--- src/components/Games/Chopsticks/Chopsticks.js
+++ src/components/Games/Chopsticks/Chopsticks.js
@@ -1,4 +1,22 @@
-import React from 'react'
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { InlineMath, BlockMath } from 'react-katex';
+import math from 'remark-math'
+import 'katex/dist/katex.min.css';
+
+import Content from './Content.js';
+
+const sectionTitleStyle = {
+    fontFamily: 'AudioWide',
+    fontSize: '2rem'
+}
+
+const renderers = {
+    inlineMath: ({ value }) => <InlineMath math={value} />,
+    math: ({ value }) => <BlockMath math={value} />
+}
+
+const seperator = <div className="my-4 py-2" />;
 
 const Chopsticks = () => {
     return (
@@ -6,7 +24,13 @@ const Chopsticks = () => {
             <h1 className="text-center" style={{ fontSize: '3rem', fontFamily: 'AudioWide' }}>CHOPSTICKS</h1>
             <hr className='mb-5' />
             <div className='container mb-5' style={{ fontSize: '1.3rem' }}>
-                Content and styling here
+                <h2 style={sectionTitleStyle}>📜 INTRODUCTION AND RULES</h2>
+                <hr className='mb-4' />
+                <ReactMarkdown source={Content.introAndRules} />
+                <img src={require('./Images/11.png')} alt="" />
+
+                {seperator}
+
             </div>
         </div>
     )
---
+++ src/components/Games/Chopsticks/Content.js
@@ -0,0 +1,12 @@
+const Content = {
+    introAndRules:
+        `
+Chopsticks is a game of strategy as well as basic math. It is also known as Finger Chess, Swords, Split, Magic Fingers, Chinese Fingers, 
+Cherries, Sticks, and Twiddly Dinks. There are many variations of rules though the overall theory and spirit of the game remain the same.
+
+Both players start with both hands holding up one finger each.
+*The hands on the right side of the pictures below used for explaining the game belong to player 1 and the hands on the left belong to player 2.*
+    `
+};
+
+export default Content;
